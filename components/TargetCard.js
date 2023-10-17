582169d7e7e11b0632e2de090379b0d83a8105f6
aldo555 <aldodumitrescu@gmail.com>
2023-10-17 19:16:43 +0300

    landing finished, responsive, navbar tweaked

---
+++ components/TargetCard.js
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+const TargetCard = ({ className, ...props }) => (
+  <div className={`${className} relative flex flex-col items-center justify-start h-80 py-5 px-7 rounded-3xl border border-cf-gray-600 shadow-sm`}>
+    <Image
+      className="h-[74px] w-auto"
+      src={`/${props.icon}`}
+      alt={props.alt}
+      priority={true}
+      width={74}
+      height={74}
+    />
+    <span className="text-xl text-cf-blue-900 mt-5" dangerouslySetInnerHTML={{__html: props.description}}></span>
+  </div>
+)
+
+export default TargetCard
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        blob: "blob 45s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": { transform: "scale(1)" },
+          "33%": { transform: "scale(1.1)" },
+          "66%": { transform: "scale(0.9)" },
+          "100%": { transform: "scale(1)" },
+        }
+      },
       colors: {
         // brand colours
         'cf-blue': {
